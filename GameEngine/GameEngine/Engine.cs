2f056db80017f5d6a9b2c8790ceafb0fb68602b6
CommName <CommName@users.noreply.github.com>
2018-11-06 13:30:23 +0100

    New class desing

--- GameEngine/GameEngine/Engine.cs
+++ GameEngine/GameEngine/Engine.cs
@@ -15,31 +15,51 @@ namespace GameEngine
         protected List<Karta> player1Hand;
         protected List<Karta> player2Hand;
 
-        public List<Karta> player1 { get { return player1Hand; } }
-        public List<Karta> player2 { get { return player2Hand; } }
+        protected List<Karta> poslednjeBacene;
+
+        public PlayerUser player1 { get; set; } 
+        public PlayerUser player2 { get; set; }
+        public PlayerUser current { get; set; }
         public Karta talon { get { return topCard; } }
 
         public Engine() {
             deck = new Spil(true);
             player1Hand = new List<Karta>();
             player2Hand = new List<Karta>();
+
+            player1 = new PlayerUser();
+            player2 = new PlayerUser();
+
+            player1.nextPlayer = player2;
+            player2.nextPlayer = player1;
+
+            player1.SetRuka(player1Hand);
+            player2.SetRuka(player2Hand);
+
             topCard = deck.Karte[0];
             deck.Karte.RemoveAt(0);
+
             //Dealibg cards
             for(int i = 0; i < 2; i++)
-            {
-      
+            {      
                 player1Hand.AddRange(deck.Karte.GetRange(0, 3));
                 deck.Karte.RemoveRange(0, 3);
                 player2Hand.AddRange(deck.Karte.GetRange(0, 3));
                 deck.Karte.RemoveRange(0, 3);
             }
 
+            current = player1;
+
         }
 
         bool Game()
         {
+            if (gameOver())
+                return false;
 
+            current.Bacenekarte(poslednjeBacene);
+            current.findBestMoce();
+            poslednjeBacene= current.BestMove.Karte;
 
 
             return true;
@@ -78,23 +98,10 @@ namespace GameEngine
         }
 
 
-        public void autoMovePlayer1()
-        {
-           
-        }
-        public void autoMovePlayer2()
-        {
 
-        }
-        public void manualMovePlayer1(TipPoteza type,List<Karta> cards,Boja color)
-        {
 
-        }
-        public void manualMovePlayer2(TipPoteza type, List<Karta> cards, Boja color)
-        {
 
-        }
-
+        /*
         public static string handToString(List<Karta> hand)
         {
             string ret = "";
@@ -104,7 +111,8 @@ namespace GameEngine
             }
             return ret;
         }
+        */
 
-
+        
     }
 }
---
+++ GameEngine/GameEngine/PlayerUser.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TIG.AV.Karte;
+namespace GameEngine
+{
+    public class PlayerUser : TIG.AV.Karte.IIgra 
+    {
+
+        #region atributi
+        protected List<Karta> hand;
+        public IMove BestMove { get; set; }
+
+       // public bool playtime { get; set; }
+        public PlayerUser nextPlayer { get; set; }
+        public PlayerUser previousPlayer { get; set; }
+
+        #endregion
+
+        #region construcotrs
+        public PlayerUser()
+        {
+         //   playtime = false;
+            
+        }
+        #endregion
+
+
+        #region interfejs
+        public void findBestMoce()
+        {
+            return;
+        }
+
+
+        public void Bacenekarte(List<Karta> karte)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BeginBestMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndBestMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void KupioKarte(List<Karta> karte)
+        {
+            hand.AddRange(karte);
+        }
+
+        public void Reset()
+        {
+            hand = new List<Karta>();
+        }
+
+        public void SetRuka(List<Karta> karte)
+        {
+            hand = karte;
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            string ret = "hand: ";
+            foreach(Karta karta in hand)
+            {
+                ret += "|" +karta.Broj + " "+karta.Boja+"|";
+            }
+
+            return ret;
+        }
+    }
+}
